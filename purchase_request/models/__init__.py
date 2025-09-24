3a01c30e12a62df5fa92877bfc6660ab256ed5f5
dbrogan-osi <dbrogan@opensourceintegrators.com>
2025-09-24 14:01:36 +0000

    Fix for ticket 66550: Submodule update
    Automated changes for ticket resolution

--- purchase_request/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import orderpoint
-from . import purchase_request
-from . import procurement_rule
-from . import product_template
-from . import purchase_order
-from . import stock_move
-from . import stock_picking
-
