212a6371594451d1e1ad0795b1b917d83683bc9c
dbrogan-osi <dbrogan@opensourceintegrators.com>
2025-09-24 19:17:54 +0000

    Fix for ticket 66577: Submodule update
    Automated changes for ticket resolution

--- purchase_landed_cost/readme/ROADMAP.rst
+++
@@ -1,2 +0,0 @@
-* Ability to add expenses in multi currency.
-* Purchase distribution report.
--- purchase_landed_cost/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import picking_import_wizard
-from . import import_invoice_line
-from . import import_landed_cost_pickings_wizard
