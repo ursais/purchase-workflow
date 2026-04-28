c3d3542942dc7b073ab69af5a37249fda7e1a47f
Balaji Kannan <bkannan@opensourceintegrators.com>
2026-04-28 21:39:49 +0000

    Tier Validation migraiton

---
+++ purchase_request_tier_validation/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019-2020 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Request Tier Validation",
+    "summary": "Extends the functionality of Purchase Requests to "
+    "support a tier validation process.",
+    "version": "19.0.1.0.0",
+    "category": "Purchase Management",
+    "website": "https://github.com/OCA/purchase-workflow",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["purchase_request", "base_tier_validation"],
+    "data": [
+        "views/purchase_request_view.xml",
+    ],
+    "demo": [
+        "demo/tier_definition.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
